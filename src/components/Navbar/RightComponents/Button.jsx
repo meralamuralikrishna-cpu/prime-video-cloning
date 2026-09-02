0c488799692f93cbfc4fd7016a530a27e3fd35e2
merala murali krishna <meralamuralikrishna@gmail.com>
2026-09-02 10:31:02 +0530

    pookie

--- src/components/Navbar/RightComponents/Button.jsx
+++ src/components/Navbar/RightComponents/Button.jsx
@@ -3,7 +3,7 @@ const Button = () => {
     <button className="bg-gray-300 px-5 py-3 font-bold text-black transition hover:bg-blue-500 hover:text-white rounded-xl">
       Join Prime
     </button>
-  );
+  )
 };
 
 export default Button;
